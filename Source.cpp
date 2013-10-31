0e5a96005f0e2d4cb394e6e89139596440a0e944
keeriex3 <shastopk@wit.edu>
2013-10-31 09:59:22 -0400

    News

---
+++ Source.cpp
@@ -0,0 +1,74 @@
+/*aesar.cpp
+Caesar’s Cipher-encrypt a plain text message by adding a shift
+to each alphabetic character to produce cipher text.
+The plain text will be in an existing file and the cipher
+text will be saved in a new file.
+*/
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void process(ifstream & in, ofstream & out);
+
+int main(){
+	/*Asks user for filenames for numbers and then only positive numbers
+	Opens streams to the files
+	Asks user for the shift
+	Calls process()
+	Closes files
+	*/
+	char inFileName[200];
+	char outFileName[200];
+	cout << "Enter file name of all numbers file including path from drive letter: \n";
+	cin >> inFileName;
+	cin.getline(inFileName, 200);
+	cout << "Enter name and path for positive numbers file: \n";
+	cin >>outFileName;
+	cin.getline(outFileName, 200);
+	cout << "So, I will read all numbers from " << inFileName << endl;
+	cout << "and write the positive numbers to " << outFileName << endl;
+	ifstream in;
+	ofstream out;
+	in.open(inFileName);
+	if (!in.is_open()){
+		cout << "ERROR: Failed to open " << inFileName << "for input \n";
+		exit(-1);
+
+	}
+	out.open(outFileName);
+	if (!out.is_open()){
+		cout << "EROR: Failed to open " << outFileName << "for output \n";
+		exit(-2);
+	}
+	process(in, out);
+	in.close();
+	out.close();
+	cout << "Look in " << outFileName << "for positive numbers\n";
+	return 0;
+}
+
+/*void process(ifstream & in, ofstream & out, int shift){
+	char c;
+	while (in >> noskipws >> c){
+		if (c >= '1' && c <= 'Z'){
+			c += shift;
+		}
+		else if (c >= 'a' && c <= 'z'){
+			c += shift;
+		}
+}
+
+out << c;
+}
+*/
+void process(ifstream & in, ofstream & out)
+{
+	double current_number;
+	while (in >> current_number){
+		if (current_number > 0)
+			out << current_number;
+
+	}
+
+}
